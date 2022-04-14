98b7e3c509d3b301853d055f2a8425dbb8e063d1
David White-Goode <davidwhite-goode@Davids-MacBook-Air.local>
2022-04-14 15:31:34 -0400

    hello world

--- seed/data.js
+++ seed/data.js
@@ -2,11 +2,11 @@ import db from "../db/connection.js"
 import OrganModel from "../models/organs.js"
 import FoodModel from "../models/foods.js"
 import organs from "./data.json" assert {type: 'json'}
-import foods from "./datafruits.json" assert { type: 'json'}
+import foods from "./datafruits.json" assert {type: 'json'}
 
 
 const seedData = async () => {
-  await db.deleteMany()
+  await OrganModel.deleteMany()
 
   await OrganModel.insertMany(organs)
   console.log('seeded, success!')
@@ -16,7 +16,7 @@ const seedData = async () => {
 seedData();
 
 const seedFoodData = async () => {
-  await db.deleteMany()
+  await FoodModel.deleteMany()
 
   await FoodModel.insertMany(foods)
   console.log('food seeded!')
