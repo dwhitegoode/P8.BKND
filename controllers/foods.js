e2754634617c10a06936a3c9672d05577da3741e
Nasim Akbor <nasimakbor@Nasims-MacBook-Air.local>
2022-04-14 15:35:36 -0400

    Added Update route, currently replaces

--- controllers/foods.js
+++ controllers/foods.js
@@ -9,3 +9,14 @@ export const getFood = async (req, res) => {
     res.json({ error: err.message })
   }
 }
+
+export const updateFood = async (req, res) => {
+  try {
+    const query = { name: `${req.params.organ}` };
+    const food = await Food.findOneAndUpdate(query, req.body)
+    res.status(200).json(food)
+  } catch (err) {
+    console.log(err)
+    res.json({ error: err.message })
+  }
+}
--- routes/foods.js
+++ routes/foods.js
@@ -4,5 +4,6 @@ import * as controllers from "../controllers/foods.js";
 const router = Router()
 
 router.get('/foods', controllers.getFood)
+router.post('/foods/:organ', controllers.updateFood)
 
 export default router;
