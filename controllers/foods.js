9d6abe132ea92e144e0cb3b7e407694dc7fbdaf1
Nasim Akbor <nasimakbor@Nasims-MacBook-Air.local>
2022-04-14 16:01:46 -0400

    fixed updatemethod

--- controllers/foods.js
+++ controllers/foods.js
@@ -13,7 +13,7 @@ export const getFood = async (req, res) => {
 export const updateFood = async (req, res) => {
   try {
     const query = { name: `${req.params.organ}` };
-    const food = await Food.findOneAndUpdate(query, req.body)
+    const food = await Food.updateMany(query, { $push: (req.body) })
     res.status(200).json(food)
   } catch (err) {
     console.log(err)
