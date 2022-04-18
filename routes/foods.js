66ca5ad3992cfbe228aab3217734d9f8a2af86ab
David White-Goode <davidwhite-goode@Davids-MacBook-Air.local>
2022-04-18 15:10:32 -0400

    save

--- routes/foods.js
+++ routes/foods.js
@@ -5,6 +5,6 @@ const router = Router()
 
 router.get('/foods', controllers.getFood)
 router.get('/foods/:organ', controllers.getSpecFood)
-router.put('/foods/:id', controllers.updateFood)
+router.put('/foods/:organ', controllers.updateFood)
 
 export default router;
