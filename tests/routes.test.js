1af6bd8196a0a926a498d386a62b6ad3e63315b5
David White-Goode <davidwhite-goode@davids-air.myfiosgateway.com>
2022-04-19 17:14:14 -0400

    push jest backend test for get, get, post

--- tests/routes.test.js
+++ tests/routes.test.js
@@ -2,10 +2,8 @@ import request from 'supertest'
 import app from '../app.js'
 import mongoose from 'mongoose'
 import Organ from '../models/organs.js'
-import Foods from '../models/foods.js'
-import db from "../db/connection.js"
 import OrganModel from "../models/organs.js"
-import FoodModel from "../models/foods.js"
+
 
 
 
@@ -31,7 +29,7 @@ beforeAll(async () => {
   }
 })
 //let projectId;
-let organName;
+let organName
 //let projectId;
 
 // projects api tests
@@ -42,27 +40,27 @@ describe('Test the express routes for organs', () => {
     // test that the status code is 200 - successful
     expect(res.statusCode).toEqual(200)
     // test that the response object has an _id property
-    // expect(res.body[0]).toHaveProperty('name')
-    //  organName = res.body[0].name
+    expect(res.body[0]).toHaveProperty('name')
+    organName = res.body[0].name
     //projectId = res.body[0]._id
     // console.log(organName)
-  })
-  //   it('should get a specific organ', async () => {
-  //     const res = await request(app).get(`/organ-api/organs/${organName}`)
-  //     expect(res.statusCode).toEqual(200)
-  //     expect(res.body).toHaveProperty('name')
-  //   })
-  // it('should creaet a new organ object', async () => {
-  //   const res = await request(app).post(`/organ-api/organs`).send({
-  //     name: "Heart",
-  //     description: "Your heart is the primary organ of your circulatory system. ",
-  //     system: "Cardio",
-  //     symptom: ["heard disease"],
-  //     images: "https://www.pngfind.com/pngs/m/28-286612_human-body-heart-anatomy-orga"
-  //   })
-  //   expect(res.statusCode).toEqual(200)
-  //   expect(res.body).toHaveProperty('name')
-  // })
+  }),
+    it('should get a specific organ', async () => {
+      const res = await request(app).get(`/organ-api/organs/${organName}`)
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('name')
+    }),
+    it('should creaet a new organ object', async () => {
+      const res = await request(app).post(`/organ-api/organs`).send({
+        name: "Heart",
+        description: "Your heart is the primary organ of your circulatory system. ",
+        system: "Cardio",
+        symptom: ["heard disease"],
+        images: "https://www.pngfind.com/pngs/m/28-286612_human-body-heart-anatomy-orga"
+      })
+      expect(res.statusCode).toEqual(200)
+      expect(res.body).toHaveProperty('name')
+    })
 })
   //,
   // Save the _id value for later use with other tests
